0bb070843e230a0f7fc81c4cca847b43f7d7a41c
seanlin2014 <seanlin12345@gmail.com>
2015-04-25 09:30:01 +0800

    add new function

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int mirror(int room[][])
+
 int main(int argc, char const *argv[])
 {
 	int w, d;
@@ -14,5 +16,34 @@ int main(int argc, char const *argv[])
 			scanf("",room[i][j]);
 		}
 	}
+	for (i = d; i >= 0 ; i--)
+	{
+		for (j = w; j >= 0 ; j--)
+		{
+			mirror(i, j);
+		}
+	}
 	return 0;
 }
+
+int mirror(int i, int j)
+{
+	(room[][] == )
+	{
+
+	}	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
